3ca366646bab6e25252ae20cbb208c731102e073
Jason LI <kahei.li@adevinta.com>
2025-07-25 12:45:55 +0200

    rename the project

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config } from "@web42-ai/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
